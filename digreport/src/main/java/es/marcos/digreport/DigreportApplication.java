c121763ece5240c5dbaeafd3122e8cabeb9c653c
marcosvarela5 <marcosviesmugardos@gmail.com>
2025-08-15 05:25:39 +0200

    Initial commit

---
+++ digreport/src/main/java/es/marcos/digreport/DigreportApplication.java
@@ -0,0 +1,13 @@
+package es.marcos.digreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigreportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigreportApplication.class, args);
+    }
+
+}
---
+++ digreport/src/test/java/es/marcos/digreport/DigreportApplicationTests.java
@@ -0,0 +1,13 @@
+package es.marcos.digreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigreportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
